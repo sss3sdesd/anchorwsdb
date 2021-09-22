94601e18e2915e779e9c1a79175d04332d189c37
s <s@x.com>
2021-09-22 17:10:46 +0800

    init

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'l',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'ws/db',
+        'reference' => 'cbb066e9bd078ec23f64c5db711fc9885a5b7f8d',
+        'name' => 'anchorws/livedirector',
         'dev' => true,
     ),
     'versions' => array(
-        'ws/db' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'anchorws/livedirector' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'l',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'cbb066e9bd078ec23f64c5db711fc9885a5b7f8d',
             'dev_requirement' => false,
         ),
     ),
